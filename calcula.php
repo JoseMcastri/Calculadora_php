e9c2cd2d7c97dffbd32c0a45306f5dcb1636ca9d
JoseMcastri <josecastri2006@gmail.com>
2022-09-17 16:33:00 -0500

    Calculadora php

---
+++ calcula.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="calculadora.css">
+    <title>calculadora</title>
+</head>
+<body>
+
+    
+        <div class="calculo">
+            
+                
+
+                <div class="suma">
+
+                <fieldset>
+                    <h2 class="simbolosuma">Suma</h2>
+
+                    <input type="number" name="" id="" placeholder="Ingrese un numero" class="numero">
+
+                    <p class="simbolo">+</p>
+
+                    <input type="number" name="" id="" placeholder="Ingrese un numero" class="numero">
+
+                    <div>
+                    <input type="submit" value="Resultado" class="resultado">
+                    </div>
+
+                    <div class="resultados"><input type="text"></div>
+
+                    </fieldset>
+                </div>
+
+
+
+
+                 <div class="resta">
+
+                <fieldset>
+                    <h2 class="simboloresta">Resta</h2>
+
+                    <input type="number" name="" id="" placeholder="Ingrese un numero" class="numero">
+
+                    <p class="simbolo-">-</p>
+
+                    <input type="number" name="" id="" placeholder="Ingrese un numero" class="numero">
+
+                    <div>
+                    <input type="submit" value="Resultado" class="resultado">
+                    </div>
+
+                    <div><input type="text"></div>
+
+                    </fieldset>
+                </div>
+
+
+
+
+
+                 <div class="multiplicacion">
+
+                <fieldset>
+                    <h2 class="simbolomulti">Multiplicacion</h2>
+
+                    <input type="number" name="" id="" placeholder="Ingrese un numero" class="numero">
+
+                    <p class="simboloX">X</p>
+
+                    <input type="number" name="" id="" placeholder="Ingrese un numero" class="numero">
+
+                    <div>
+                    <input type="submit" value="Resultado" class="resultado">
+                    </div>
+
+                    <div><input type="text"></div>
+
+                    </fieldset>
+                </div>
+
+
+
+
+                 <div class="division">
+
+                <fieldset>
+                    <h2 class="simbolodiv">Division</h2>
+
+                    <input type="number" name="" id="" placeholder="Ingrese un numero" class="numero">
+
+                    <p class="simbolo-division">/</p>
+
+                    <input type="number" name="" id="" placeholder="Ingrese un numero" class="numero">
+
+                    <div>
+                    <input type="submit" value="Resultado" class="resultado">
+                    </div>
+
+                    <div><input type="text"></div>
+
+                    </fieldset>
+                </div>
+            
+        </div>
+    
+        
+    
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Iniciar sesion</title>
+</head>
+<body>
+    
+
+    <fieldset> 
+        <form action="calcula.php" method="post">
+    <div class="login">  
+
+        <h2 class="Sesion">Inicia sesion</h2>
+
+        <div class="centrar">
+
+        <div class="usuario">
+            <input type="text" class="Usua">
+            <label for="">Usuario</label>
+        </div>
+
+        <div class="contraseña">
+            <input type="password" class="Contra">
+            <label for="">Contraseña</label>
+        </div>
+
+        <div class="ingresar">
+
+        
+
+        <input type="submit" value="->" class="Ingresa">
+        </div>
+        
+        
+        </div>
+    </div>    
+    </form>
+    </fieldset>
+
+
+</body>
+</html>
